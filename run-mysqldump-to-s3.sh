7580fd13c8197770b6a7f0415cbc476183d1c797
prasitstk <prasit.stk@gmail.com>
2022-12-04 02:11:26 +0000

    Remove unnecessary code

--- run-mysqldump-to-s3.sh
+++ run-mysqldump-to-s3.sh
@@ -11,21 +11,12 @@ export MYSQL_PWD=$MYSQL_PASSWORD
 mysqldump -P $MYSQL_PORT -h $MYSQL_HOST -u $MYSQL_USERNAME --ssl-mode=$MYSQL_SSL_MODE $MYSQL_DB_NAME > /tmp/dump.sql
 echo $(date -u '+%Y-%m-%d %H:%M:%S') - Finish creating MySQL dump file.
 
-# remove the following lines if they exist:
-#   SET @@SESSION.SQL_LOG_BIN= 0;
-#   SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
-#   SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
-echo $(date -u '+%Y-%m-%d %H:%M:%S') - Start removing some lines...
-sed '/^SET @@GLOBAL.GTID_PURGED/d' /tmp/dump.sql > /tmp/dump-edited-01.sql
-sed '/^SET @@SESSION.SQL_LOG_BIN/d' /tmp/dump-edited-01.sql > /tmp/dump-edited-final.sql
-echo $(date -u '+%Y-%m-%d %H:%M:%S') - Finish removing some lines.
-
 # gzip
 echo $(date -u '+%Y-%m-%d %H:%M:%S') - Start gzipping the dump file...
-gzip /tmp/dump-edited-final.sql
+gzip /tmp/dump.sql
 echo $(date -u '+%Y-%m-%d %H:%M:%S') - Finish gzipping the dump file.
 
 # upload to S3 bucket
 echo $(date -u '+%Y-%m-%d %H:%M:%S') - Start uploading the dump file to "'$AWS_S3_BUCKET_NAME'" bucket...
-aws s3api put-object --bucket $AWS_S3_BUCKET_NAME --key "db-dumps/db-dump_${MYSQL_DB_NAME}_$(date -u '+%Y-%m-%d_%H-%M-%S')-sql.gz" --body /tmp/dump-edited-final.sql.gz
+aws s3api put-object --bucket $AWS_S3_BUCKET_NAME --key "db-dumps/db-dump_${MYSQL_DB_NAME}_$(date -u '+%Y-%m-%d_%H-%M-%S')-sql.gz" --body /tmp/dump.sql.gz
 echo $(date -u '+%Y-%m-%d %H:%M:%S') - Finish uploading the dump file.
